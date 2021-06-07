78ab9d1e78e8caa26e4c360db1304dd8ed5061e1
mlvrkhn <martin.guide@gmail.com>
2021-06-07 13:24:08 +0200

    Move validation to hook

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,56 +1,60 @@
-import React, { useState } from 'react';
+import React from 'react';
 import CreditForm from './CreditForm';
 
+import useValidation from '../hooks/useValidation';
+
 const Card = () => {
-    const [ error, setError ] = useState('');
-    const [ cardIssuer, setCardIssuer ] = useState('');
+    
+    // const [ cardIssuer, setCardIssuer ] = useState('');
 
-    const validateCredit = (cardNum) => {
-        const oddDgtSum = cardNum
-			.split('')
-			.reverse()
-			.filter((_, i) => i % 2 !== 1)
-            .map(d => parseInt(d))
-			.reduce((a, b) => a + b);
+    const [cardIssuer, error, validateCredit] = useValidation()
+
+    // const validateCredit = (cardNum) => {
+    //     const oddDgtSum = cardNum
+	// 		.split('')
+	// 		.reverse()
+	// 		.filter((_, i) => i % 2 !== 1)
+    //         .map(d => parseInt(d))
+	// 		.reduce((a, b) => a + b);
             
-        const evenDgtSum = cardNum
-			.split('')
-			.reverse()
-			.filter((_, i) => i % 2 === 1)
-			.map(d => (d * 2).toString().split(''))
-			.flat()
-            .map(d => parseInt(d))
-            .reduce((a, b) => a + b);
+    //     const evenDgtSum = cardNum
+	// 		.split('')
+	// 		.reverse()
+	// 		.filter((_, i) => i % 2 === 1)
+	// 		.map(d => (d * 2).toString().split(''))
+	// 		.flat()
+    //         .map(d => parseInt(d))
+    //         .reduce((a, b) => a + b);
         
-        if ((oddDgtSum + evenDgtSum) % 10 !== 0) {
-			setError('Invalid card number');
-		}
+    //     if ((oddDgtSum + evenDgtSum) % 10 !== 0) {
+	// 		setError('Invalid card number');
+	// 	}
         
-        const twoFirstCardDigits = parseInt(cardNum.slice(0, 2));
+    //     const twoFirstCardDigits = parseInt(cardNum.slice(0, 2));
 
-        switch (cardNum.length) {
-			case 13:
-                if (twoFirstCardDigits >= 40 && twoFirstCardDigits < 50) {
-                    setCardIssuer('VISA')
-                }
-				return;
-			case 15:
-                if (twoFirstCardDigits === 34 || twoFirstCardDigits === 37) {
-                    setCardIssuer('AmEx')
-                }
-				return;
-            case 16:
-                if (twoFirstCardDigits >= 40 && twoFirstCardDigits < 50) {
-					setCardIssuer('Visa');
-				};
-                if (twoFirstCardDigits >= 51 && twoFirstCardDigits <= 55) {
-                    setCardIssuer('MasterCard')
-                };
-                return;
-			default:
-				return;
-        };
-    }
+    //     switch (cardNum.length) {
+	// 		case 13:
+    //             if (twoFirstCardDigits >= 40 && twoFirstCardDigits < 50) {
+    //                 setCardIssuer('VISA')
+    //             }
+	// 			return;
+	// 		case 15:
+    //             if (twoFirstCardDigits === 34 || twoFirstCardDigits === 37) {
+    //                 setCardIssuer('AmEx')
+    //             }
+	// 			return;
+    //         case 16:
+    //             if (twoFirstCardDigits >= 40 && twoFirstCardDigits < 50) {
+	// 				setCardIssuer('Visa');
+	// 			};
+    //             if (twoFirstCardDigits >= 51 && twoFirstCardDigits <= 55) {
+    //                 setCardIssuer('MasterCard')
+    //             };
+    //             return;
+	// 		default:
+	// 			return;
+    //     };
+    // }
     
     if (error) {
         return <h1>Invalid card number</h1>;
