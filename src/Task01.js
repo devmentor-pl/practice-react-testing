34f238720dcc85246de2e3641e4d736a2de9ff25
Rafał Kazik <kazik.raf@gmail.com>
2022-04-23 19:25:30 +0200

    1st task

--- src/Task01.js
+++ src/Task01.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import LoginForm from './components/LoginForm';
+import CatchError from './CatchError';
 
 const Task01 = () => {
-    function tryAuth(login, password) {
-        return login.length + password.length > 6;
-    }
+  function tryAuth(login, password) {
+    return login.length + password.length > 6;
+  }
 
-    return (
-        <section>
-            <h1>Task01</h1>
-            <LoginForm tryAuth={ tryAuth }/>
-        </section>
-    );
+  return (
+    <CatchError>
+      <section>
+        <h1>Task01</h1>
+        <LoginForm tryAuth={tryAuth} />
+      </section>
+    </CatchError>
+  );
 };
 
 export default Task01;
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,69 +1,103 @@
 import React, { useState } from 'react';
 
 function LoginForm(props) {
-    const userDefault = {
-        login: {
-            value: '',
-            error: '',
-        },
-        password: {
-            value: '',
-            error: '',
-        }
+  const userDefault = {
+    login: {
+      value: '',
+      error: '',
+    },
+    password: {
+      value: '',
+      error: '',
+    },
+  };
+
+  const wrongInputValueStyle = {
+    display: 'none',
+  };
+  const correctInputValueStyle = {
+    display: 'block',
+  };
+
+  const [user, setUser] = useState(userDefault);
+
+  function checkValue(value) {
+    if (value.length <= 3) {
+      throw new Error('The field is too short!');
     }
+  }
 
-    const [user, setUser] = useState(userDefault);
-
-    function checkValue(value) {
-        if(value.length <= 3)  {
-            throw new Error('The field is too short!');
-        }
+  function handleChange(e) {
+    const { name: field, value } = e.target;
+    if (typeof user[field] !== 'undefined') {
+      try {
+        checkValue(value);
+        setUser({ ...user, [field]: { value, error: '' } });
+      } catch (err) {
+        setUser({ ...user, [field]: { value, error: err.message } });
+      }
     }
+  }
 
-    function handleChange(e) {
-        const {name: field, value} = e.target;
-        if(typeof user[field] !== 'undefined') {
-            checkValue(value);
-            setUser({...user, [field]: {value, error: ''} });
-        }
+  const [hasError, setError] = useState(false);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const { tryAuth } = props;
+    const { login, password } = e.target.elements;
+
+    const authResp = tryAuth(login.value, password.value);
+    if (typeof authResp.then === 'function') {
+      // if return Promise
+      authResp.catch(() => setError(true));
     }
-
-    function throwError() {
-        throw new Error('Incorrect data!');
+    if (!authResp) {
+      setError(true);
     }
-
-    function handleSubmit(e) {
-        e.preventDefault();
-
-        const {tryAuth} = props;
-        const {login, password} = e.target.elements;
-
-        const authResp = tryAuth(login.value, password.value);
-        if(typeof authResp.then === 'function') { // if return Promise
-            authResp.catch(() => throwError() );
-        } else if(!authResp) {
-            throwError()
-        }
+    if (authResp) {
+      setError(false);
     }
+  }
 
-    const {login, password} = user;
-    return (
-        <form onSubmit={ handleSubmit }>
-            <p>
-                <label>
-                    login: <input name="login" value={ login.value } onChange={e => handleChange(e)} />
-                    { login.error && <strong>{ login.error }</strong> }
-                </label>
-            </p>
-            <p>
-                <label>
-                    password: <input name="password" value={ password.value } onChange={e => handleChange(e)} />
-                    { password.error && <strong>{ password.error }</strong> }
-                </label>
-            </p>
-            <p><button>send</button></p>
-        </form>
-    );
+  const { login, password } = user;
+  return (
+    <>
+      <form
+        style={hasError ? wrongInputValueStyle : correctInputValueStyle}
+        onSubmit={handleSubmit}
+      >
+        <p>
+          <label>
+            login:{' '}
+            <input
+              name='login'
+              value={login.value}
+              onChange={(e) => handleChange(e)}
+            />
+            {login.error && <strong>{login.error}</strong>}
+          </label>
+        </p>
+        <p>
+          <label>
+            password:{' '}
+            <input
+              name='password'
+              value={password.value}
+              onChange={(e) => handleChange(e)}
+            />
+            {password.error && <strong>{password.error}</strong>}
+          </label>
+        </p>
+        <p>
+          <button>send</button>
+        </p>
+      </form>
+      <h1 style={hasError ? correctInputValueStyle : wrongInputValueStyle}>
+        Something went wrong :/{' '}
+      </h1>
+    </>
+  );
 }
 
 export default LoginForm;
