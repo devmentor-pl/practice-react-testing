cfe42a3c9baaf3f676625670b2d7e4a12b24a332
Katarzyna Panz <katarzyna.panz@gmail.com>
2023-02-17 11:04:07 +0100

    Add solution to exercise 01

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -2,29 +2,36 @@ import React, { useState } from 'react';
 import users from './../db/users';
 import {getMd5} from './../providers/md5Provider';
 import LoginForm from './LoginForm';
+import CatchError from "./CatchError";
 
 const Auth = () => {
-    const [user, setUser] = useState(null);
+  const [user, setUser] = useState(null);
 
-    async function findUser(login, password) {
-        const md5 = await getMd5(password);
+  async function findUser(login, password) {
+    const md5 = await getMd5(password);
 
-        console.log(md5, login);
+    console.log(md5, login);
 
-        const userFromDB = users.find(u => u.login === login && u.password === md5);
-        if(userFromDB) {
-            setUser(userFromDB);
-            return userFromDB;
-        }
-
-        return null;
+    const userFromDB = users.find(
+      (u) => u.login === login && u.password === md5
+    );
+    if (userFromDB) {
+      setUser(userFromDB);
+      return userFromDB;
     }
 
-    if(user) {
-        return <h1>Jesteś zalogowany jako: {user.login}</h1>
-    }
+    return null;
+  }
 
-    return <LoginForm tryAuth={findUser} />
+  if (user) {
+    return <h1>Jesteś zalogowany jako: {user.login}</h1>;
+  }
+
+  return (
+    <CatchError>
+      <LoginForm tryAuth={findUser} />
+    </CatchError>
+  );
 };
 
 export default Auth;
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,69 +1,95 @@
 import React, { useState } from 'react';
 
 function LoginForm(props) {
-    const userDefault = {
-        login: {
-            value: '',
-            error: '',
-        },
-        password: {
-            value: '',
-            error: '',
-        }
-    }
+  const userDefault = {
+    login: {
+      value: "",
+      error: "",
+    },
+    password: {
+      value: "",
+      error: "",
+    },
+  };
 
-    const [user, setUser] = useState(userDefault);
+  const [user, setUser] = useState(userDefault);
 
-    function checkValue(value) {
+  /*function checkValue(value) {
         if(value.length <= 3)  {
             throw new Error('The field is too short!');
         }
+    }*/
+
+  function checkValue(value) {
+    if (value.length <= 3) {
+      throw new Error("The field is too short!");
     }
+  }
 
-    function handleChange(e) {
-        const {name: field, value} = e.target;
-        if(typeof user[field] !== 'undefined') {
-            checkValue(value);
-            setUser({...user, [field]: {value, error: ''} });
-        }
+  function handleChange(e) {
+    const { name: field, value } = e.target;
+    let error = "";
+    console.log(e.target);
+    if (typeof user[field] !== "undefined") {
+      try {
+        checkValue(value);
+      } catch (err) {
+        console.log(err.message);
+        error = err.message;
+      }
+      setUser({ ...user, [field]: { value, error } });
     }
+  }
 
-    function throwError() {
-        throw new Error('Incorrect data!');
+  function throwError() {
+    throw new Error("Incorrect data!");
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const { tryAuth } = props;
+    const { login, password } = e.target.elements;
+
+    const authResp = tryAuth(login.value, password.value);
+    if (typeof authResp.then === "function") {
+      // if return Promise
+      authResp.catch(() => throwError());
+    } else if (!authResp) {
+      throwError();
     }
+  }
 
-    function handleSubmit(e) {
-        e.preventDefault();
-
-        const {tryAuth} = props;
-        const {login, password} = e.target.elements;
-
-        const authResp = tryAuth(login.value, password.value);
-        if(typeof authResp.then === 'function') { // if return Promise
-            authResp.catch(() => throwError() );
-        } else if(!authResp) {
-            throwError()
-        }
-    }
-
-    const {login, password} = user;
-    return (
-        <form onSubmit={ handleSubmit }>
-            <p>
-                <label>
-                    login: <input name="login" value={ login.value } onChange={e => handleChange(e)} />
-                    { login.error && <strong>{ login.error }</strong> }
-                </label>
-            </p>
-            <p>
-                <label>
-                    password: <input name="password" value={ password.value } onChange={e => handleChange(e)} />
-                    { password.error && <strong>{ password.error }</strong> }
-                </label>
-            </p>
-            <p><button>send</button></p>
-        </form>
-    );
+  const { login, password } = user;
+  return (
+    <form onSubmit={handleSubmit}>
+      <p>
+        <label>
+          login:{" "}
+          <input
+            name="login"
+            value={login.value}
+            onChange={(e) => handleChange(e)}
+          />
+          {login.error && <strong>{login.error}</strong>}
+        </label>
+      </p>
+      <p>
+        <label>
+          password:{" "}
+          <input
+            name="password"
+            value={password.value}
+            onChange={(e) => handleChange(e)}
+          />
+          {password.error && <strong>{password.error}</strong>}
+        </label>
+      </p>
+      <p>
+        <button>send</button>
+      </p>
+    </form>
+  );
 }
 
 export default LoginForm;
