6a2bed0eab785bfa7bc95b174f8dcb56f1cd9ab5
Michal <maxpoz3212@gmail.com>
2023-05-31 18:33:37 +0200

    Add task01 first solution

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -23,8 +23,12 @@ function LoginForm(props) {
     function handleChange(e) {
         const {name: field, value} = e.target;
         if(typeof user[field] !== 'undefined') {
-            checkValue(value);
-            setUser({...user, [field]: {value, error: ''} });
+            try {
+              checkValue(value);
+              setUser({ ...user, [field]: { value, error: "" } });
+            } catch (err) {
+              setUser({ ...user, [field]: { value, error: err.message } });
+            }
         }
     }
 
