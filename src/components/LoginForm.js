abc9a127b1bcc7b82c6b671f540213cc62a710c4
Michal <maxpoz3212@gmail.com>
2023-06-02 17:35:55 +0200

    Add task01 second solution

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,73 +1,95 @@
 import React, { useState } from 'react';
 
 function LoginForm(props) {
-    const userDefault = {
-        login: {
-            value: '',
-            error: '',
-        },
-        password: {
-            value: '',
-            error: '',
-        }
+  const userDefault = {
+    login: {
+      value: "",
+      error: "",
+    },
+    password: {
+      value: "",
+      error: "",
+    },
+  };
+
+  const [user, setUser] = useState(userDefault);
+  const [hasError, setHasError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  function checkValue(value) {
+    if (value.length <= 3) {
+      throw new Error("The field is too short!");
     }
+  }
 
-    const [user, setUser] = useState(userDefault);
-
-    function checkValue(value) {
-        if(value.length <= 3)  {
-            throw new Error('The field is too short!');
-        }
+  function handleChange(e) {
+    const { name: field, value } = e.target;
+    if (typeof user[field] !== "undefined") {
+      try {
+        checkValue(value);
+        setUser({ ...user, [field]: { value, error: "" } });
+      } catch (err) {
+        setUser({ ...user, [field]: { value, error: err.message } });
+      }
     }
+  }
 
-    function handleChange(e) {
-        const {name: field, value} = e.target;
-        if(typeof user[field] !== 'undefined') {
-            try {
-              checkValue(value);
-              setUser({ ...user, [field]: { value, error: "" } });
-            } catch (err) {
-              setUser({ ...user, [field]: { value, error: err.message } });
-            }
-        }
+  function throwError() {
+    setHasError(true);
+    setErrorMessage("Wprowadziłeś niepoprawne dane!");
+    // z tego co rozumiem, żeby użyć .getDerivedStateFromError() musiałbym cały komponent przerobić na klasowy (próbowałem to zrobić za pomocą komponentu CatchError, ale tak jak dokumentacja Reacta mówi to nie zadziała przy event handlerach, dlatego zrobiłem w ten sposób)
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const { tryAuth } = props;
+    const { login, password } = e.target.elements;
+
+    const authResp = tryAuth(login.value, password.value);
+    if (typeof authResp.then === "function") {
+      // if return Promise
+      authResp.catch(() => throwError());
+    } else if (!authResp) {
+      throwError();
     }
+  }
 
-    function throwError() {
-        throw new Error('Incorrect data!');
-    }
+  const { login, password } = user;
 
-    function handleSubmit(e) {
-        e.preventDefault();
+  if (hasError) {
+    return <h1>{errorMessage}</h1>;
+  }
 
-        const {tryAuth} = props;
-        const {login, password} = e.target.elements;
-
-        const authResp = tryAuth(login.value, password.value);
-        if(typeof authResp.then === 'function') { // if return Promise
-            authResp.catch(() => throwError() );
-        } else if(!authResp) {
-            throwError()
-        }
-    }
-
-    const {login, password} = user;
-    return (
-        <form onSubmit={ handleSubmit }>
-            <p>
-                <label>
-                    login: <input name="login" value={ login.value } onChange={e => handleChange(e)} />
-                    { login.error && <strong>{ login.error }</strong> }
-                </label>
-            </p>
-            <p>
-                <label>
-                    password: <input name="password" value={ password.value } onChange={e => handleChange(e)} />
-                    { password.error && <strong>{ password.error }</strong> }
-                </label>
-            </p>
-            <p><button>send</button></p>
-        </form>
-    );
+  return (
+      <form onSubmit={handleSubmit}>
+        <p>
+          <label>
+            login:{" "}
+            <input
+              name="login"
+              value={login.value}
+              onChange={(e) => handleChange(e)}
+            />
+            {login.error && <strong>{login.error}</strong>}
+          </label>
+        </p>
+        <p>
+          <label>
+            password:{" "}
+            <input
+              name="password"
+              value={password.value}
+              onChange={(e) => handleChange(e)}
+            />
+            {password.error && <strong>{password.error}</strong>}
+          </label>
+        </p>
+        <p>
+          <button>send</button>
+        </p>
+      </form>
+  );
 }
 
 export default LoginForm;
