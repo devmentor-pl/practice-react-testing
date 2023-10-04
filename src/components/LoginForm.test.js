2cd41ccb5ab175b2edafa7363bcfe86e1658a10d
Diana Rasińska-Kowalczyk <diana.rasinska@gmail.com>
2023-10-04 08:51:01 +0200

    Ex 2 test 4 does not work

--- src/components/LoginForm.test.js
+++ src/components/LoginForm.test.js
@@ -4,61 +4,66 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
 describe("<LoginForm>", () => {
-	test("should render form elements", () => {
-		const mockAuth = jest.fn(() => true);
-		render(<LoginForm tryAuth={mockAuth} />);
-		const loginInput = screen.getByRole("textbox", { name: "login:" });
-		expect(loginInput).toBeInTheDocument();
-		const passwordInput = screen.getByRole("textbox", { name: "password:" });
-		expect(passwordInput).toBeInTheDocument();
-		const button = screen.getByRole("button");
-		expect(button).toBeInTheDocument();
-	});
 
-	test("should render 2 errors when each input value length is less or equal 3", () => {
-		const mockAuth = jest.fn(() => false);
-		render(<LoginForm tryAuth={mockAuth} />);
-		const loginInput = screen.getByRole("textbox", { name: "login:" });
-		const passwordInput = screen.getByRole("textbox", { name: "password:" });
-		const loginText = "abc";
-		const passwordText = "def";
-		userEvent.type(loginInput, loginText);
-		userEvent.type(passwordInput, passwordText);
-		const errors = screen.getAllByText("The field is too short!");
-		expect(errors).toHaveLength(2);
-	});
+		test("should render form elements", () => {
+			const mockAuth = jest.fn(() => true);
+			render(<LoginForm tryAuth={mockAuth} />);
+			const loginInput = screen.getByRole("textbox", { name: "login:" });
+			expect(loginInput).toBeInTheDocument();
+			const passwordInput = screen.getByRole("textbox", { name: "password:" });
+			expect(passwordInput).toBeInTheDocument();
+			const button = screen.getByRole("button");
+			expect(button).toBeInTheDocument();
+		});
 
-	test("should submit form with correct input values", () => {
-		const mockAuth = jest.fn(() => true);
-		render(<LoginForm tryAuth={mockAuth} />);
-		const loginInput = screen.getByRole("textbox", { name: "login:" });
-		const passwordInput = screen.getByRole("textbox", { name: "password:" });
-		const loginText = "abcdef";
-		const passwordText = "ghijkl";
-		userEvent.type(loginInput, loginText);
-		userEvent.type(passwordInput, passwordText);
-		mockAuth.mockReturnValueOnce(true);
-		const button = screen.getByRole("button");
-		userEvent.click(button);
-		expect(mockAuth).toBeCalled();
-	});
+		test("should render 2 errors when each input value length is less or equal 3", () => {
+			const mockAuth = jest.fn(() => false);
+			render(<LoginForm tryAuth={mockAuth} />);
+			const loginInput = screen.getByRole("textbox", { name: "login:" });
+			const passwordInput = screen.getByRole("textbox", { name: "password:" });
+			const loginText = "abc";
+			const passwordText = "def";
+			userEvent.type(loginInput, loginText);
+			userEvent.type(passwordInput, passwordText);
+			const errors = screen.getAllByText("The field is too short!");
+			expect(errors).toHaveLength(2);
+		});
 
-	test("should render error text when submitted with wrong input values", async () => {
-		const mockAuth = jest.fn();
-		render(
-			<CatchError>
-				<LoginForm tryAuth={mockAuth} />
-			</CatchError>
-		);
-		const loginInput = screen.getByRole("textbox", { name: "login:" });
-		const passwordInput = screen.getByRole("textbox", { name: "password:" });
-		const loginText = "abc";
-		const passwordText = "def";
-		userEvent.type(loginInput, loginText);
-		userEvent.type(passwordInput, passwordText);
-		const button = screen.getByRole("button");
-		userEvent.click(button);
-		const error = await screen.findByText("Incorrect data");
-		expect(error).toBeInTheDocument();
-	});
+		test("should submit form with correct input values", () => {
+			const mockAuth = jest.fn(() => true);
+			render(<LoginForm tryAuth={mockAuth} />);
+			const loginInput = screen.getByRole("textbox", { name: "login:" });
+			const passwordInput = screen.getByRole("textbox", { name: "password:" });
+			const loginText = "abcdef";
+			const passwordText = "ghijkl";
+			userEvent.type(loginInput, loginText);
+			userEvent.type(passwordInput, passwordText);
+			mockAuth.mockReturnValueOnce(true);
+			const button = screen.getByRole("button");
+			userEvent.click(button);
+			expect(mockAuth).toBeCalled();
+		});
+
+		test("should render 'Incorrect data' when submitted with wrong input values", async () => {
+			const mockAuth = jest.fn();
+			render(
+				<CatchError>
+					<LoginForm tryAuth={mockAuth} />
+				</CatchError>
+			);
+			const loginInput = screen.getByRole("textbox", { name: "login:" });
+			const passwordInput = screen.getByRole("textbox", { name: "password:" });
+			const loginText = "abc";
+			const passwordText = "def";
+			userEvent.type(loginInput, loginText);
+			userEvent.type(passwordInput, passwordText);
+			const button = screen.getByRole("button");
+			userEvent.click(button);
+			expect(mockAuth).toBeCalled(1);
+			mockAuth.mockReturnValue(false);
+			const error = await screen.findByText(/Incorrect data/i, {
+				timeout: 14000,
+			});
+			expect(error).toBeInTheDocument();
+		});
 });
