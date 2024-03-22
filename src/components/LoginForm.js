9889ea18a0c0a4838464aab41aceb8265caf0416
Kaczupinko <kaczupinko@gmail.com>
2024-03-22 14:58:49 +0000

    Fix LoginForm

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -13,51 +13,40 @@ function LoginForm(props) {
     }
 
     const [user, setUser] = useState(userDefault);
-    const [isError, setIsError] = useState(false)
+    const [isError, setIsError] = useState(false);
 
     function checkValue(value) {
         if (value.length <= 3) {
-            throw new Error('The field is too short!');
+            return 'The field is too short!';
         }
+        return '';
     }
-
-    const setUserData = (field, value, error = '') => {
-        setUser({ ...user, [field]: { value, error } });
-    }
-
 
     function handleChange(e) {
         const { name: field, value } = e.target;
         if (typeof user[field] !== 'undefined') {
-            try {
-                checkValue(value);
-                setUserData(field, value)
-            } catch (error) {
-                setUserData(field, value, error.message)
-            }
+            const errorMessage = checkValue(value);
+            setUserData(field, value, errorMessage);
         }
     }
 
-    function throwError() {
-        throw new Error('Incorrect data!');
-    }
+    const setUserData = (field, value, error = '') => {
+        setUser(prevUser => ({ ...prevUser, [field]: { value, error } }));
+    };
 
     async function handleSubmit(e) {
         e.preventDefault();
-
         const { tryAuth } = props;
         const { login, password } = e.target.elements;
 
-        const authResp = await tryAuth(login.value, password.value);
-        if (typeof authResp === 'function') { // if return Promise
-            authResp.catch(() => setIsError(true));
-        } else if (!authResp) {
-            setIsError(true)
+        try {
+            const authResp = await tryAuth(login.value, password.value);
+            if (!authResp) {
+                setIsError(true);
+            }
+        } catch (error) {
+            setIsError(true);
         }
-    }
-
-    if (isError) {
-        throwError()
     }
 
     const { login, password } = user;
@@ -65,17 +54,18 @@ function LoginForm(props) {
         <form onSubmit={handleSubmit}>
             <p>
                 <label>
-                    login: <input name="login" value={login.value} onChange={e => handleChange(e)} />
+                    login: <input name="login" value={login.value} onChange={handleChange} />
                     {login.error && <strong>{login.error}</strong>}
                 </label>
             </p>
             <p>
                 <label>
-                    password: <input name="password" value={password.value} onChange={e => handleChange(e)} />
+                    password: <input name="password" value={password.value} onChange={handleChange} />
                     {password.error && <strong>{password.error}</strong>}
                 </label>
             </p>
-            <p><button>send</button></p>
+            {isError && <p>Incorrect data or authentication failed.</p>}
+            <p><button type="submit">send</button></p>
         </form>
     );
 }
